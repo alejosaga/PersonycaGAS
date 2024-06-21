7184860af6ab5848a5f884caa99f7e058ea80a67
alejosaga <alejandrosaga61@gmail.com>
2024-06-21 15:42:22 -0500

    creacion envio de reminders

---
+++ Cotizaciones/enviarCotReminder.js
@@ -0,0 +1,25 @@
+function parseCotizacionString(cotizacionString) {
+    const SSreminders = SpreadsheetApp.openById(remidersCotId);
+    const shetReminders = SSreminders.getSheetByName(Reminders);
+    const lastRowRem = shetReminders.getLastRow();
+    const lastColumnRem = shetReminders.getLastColumn();
+
+    let  data = shetReminders.getDataRange().getValues()
+
+    // Dividir el string por las comas
+    const parts = data.split(',');
+  
+    // Crear un objeto para almacenar las partes
+    const cotizacionData = {};
+  
+    // Recorrer las partes y dividir por ": " para obtener clave y valor
+    parts.forEach(part => {
+      const [key, value] = part.split(': ').map(item => item.trim());
+      cotizacionData[key] = value;
+    });
+  
+    console.log(cotizacionData);
+  }
+  
+ 
+  
