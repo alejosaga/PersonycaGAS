c588542d80e504735d5f6cde19c0a69d077c956b
alejosaga <alejandrosaga61@gmail.com>
2024-06-21 21:31:01 -0500

    ajustes envio de reminders

--- Cotizaciones/enviarCotReminder.js
+++ Cotizaciones/enviarCotReminder.js
@@ -69,6 +69,7 @@ function parseCotizacionString() {
         let servicioInteres = null;
         emailData.forEach(emailRow => {
           if (emailRow.includes(clienteNit)) {
+            console.log(emailRow)
             email = emailRow[1]; // Asumiendo que el email está en la columna B (índice 1)
             nombreContacto = emailRow[6]; // Asumiendo que el nombre de contacto está en la columna D (índice 3)
             servicioInteres = emailRow[14]; // Asumiendo que el servicio de interés está en la columna F (índice 5)
@@ -91,7 +92,8 @@ function parseCotizacionString() {
   
           // Enviar el correo electrónico con el archivo adjunto
           MailApp.sendEmail({
-            to: email,personycaEmail1,
+            to: email,
+            cc: personycaEmail1,
             subject: `Seguimiento cotizacion ${servicioInteres} Personyca`,
             body: `Estimado ${nombreContacto},\n\nLe escribimos para retomar el proceso de cotizacion ${cotizacionValue} de ${servicioInteres}.\n\nPor favor haganos saber si pudo revisar la propuesta, si tiene alguna duda o de que manera podemos llegar a un acuerdo. Adjunto encontrará el documento con la oferta economica del servicio. En caso de estar de acuerdo con la cotización agradecemos pueda completar el formulario en el siguiente enlace: ${formLink}.\n\nAgradecemos su atención y recuerde que quedamos atentos ante cualquier consulta que pueda surgir al respecto.\n\nSaludos cordiales,\nPersonyca`,
             attachments: [file.getAs(MimeType.PDF)]
